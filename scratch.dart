e080e07fd2c3e0e10cc6d0aff2c96b107082393a
Eshaan-B <eb7127@srmist.edu.in>
2020-05-17 14:46:52 +0530

    Dependencies added

---
+++ scratch.dart
@@ -0,0 +1,34 @@
+
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String task2Result =  await task2();
+  task3(task2Result);
+}
+
+void task1() {
+
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+
+  String result;
+  Duration threeSeconds = Duration(seconds: 3);
+  await Future.delayed(threeSeconds, (){
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
+
+
+}
+
+void task3(String task2Data) {
+
+  print('Task 3 complete with $task2Data');
+}
